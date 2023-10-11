7c80855384276942856e4963a869db74fcef45eb
nostalgic-math-student <josue.rojas@ciencias.unam.mx>
2023-10-10 18:53:11 -0600

    New logo added

--- frontend-galois-guilds/src/components/Home.jsx
+++ frontend-galois-guilds/src/components/Home.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-
+import Navbar from './Navbar';
 const Home = () => {
-  return (
-    <div className=' h-screen text-center'>
-        <div className='bg-base-200 w-full h-1/2 mx-auto'> 
-        Home!     
-        </div>
-<div className='divider'></div>
-<div className='flex-grow'>
-<Outlet />
 
-</div>
+
+  return (
+    <div className="hero min-h-screen" style={{backgroundImage: 'url(gglogo.jpg)'}}>
+      <div className='hero bg-base-200 w-full h-1/2 mx-auto'>
+        <div className='hero-content text-center'>
+          <div className='max-w-md'>
+            <h1 className='text-5xl font-bold'>
+              Home!
+            </h1>
+            <p className='py-6'>
+              Come build your guild in the blockchain UwU
+            </p>
+          </div>
+
+        </div>
+      </div>
+      <div className='flex-grow'>
+        <Outlet />
+
+      </div>
     </div>
-    
+
   )
 }
 
